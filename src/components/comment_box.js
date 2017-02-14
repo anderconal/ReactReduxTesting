efe5ab605e9e2fabaf11c15a27d6bdcce7dcb2cb
Ander Conal <ander.conal@jinnapp.com>
2017-02-14 15:26:05 +0100

    Redux testing working

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-
-export default class CommentBox extends Component {
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+ 
+class CommentBox extends Component {
   constructor(props) {
     super(props);
 
@@ -14,18 +16,24 @@ export default class CommentBox extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   }
 
   render() {
     return (
       <form className="comment-box" onSubmit={this.handleSubmit.bind(this)}>
+        <h4>Add a comment</h4>
         <textarea 
           value={this.state.comment} 
           onChange={this.handleChange.bind(this)}
         />
+        <div>
         <button action="submit">Submit Comment</button>
+        </div>
       </form>
     );
   }
 }
+
+export default connect(null, actions)(CommentBox);
--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const CommentList = (props) => {
-  const list = props.comments.map(comment => <li key={comment}>{comment}></li>);
+  const list = props.comments.map(comment => <li key={comment}>{comment}</li>);
 
   return (
     <ul className="comment-list">
